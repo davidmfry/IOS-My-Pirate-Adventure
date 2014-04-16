84815e99565d30d1b681fdc51a053c4cc60752d3
David Fry <david.fry.tv@gmail.com>
2014-04-16 07:58:35 -0700

    added all the storyboard elements to the viewController header file

--- My Pirate Adventure/DF_ViewController.h
+++ My Pirate Adventure/DF_ViewController.h
@@ -9,5 +9,24 @@
 #import <UIKit/UIKit.h>
 
 @interface DF_ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *healthLabel;
+@property (strong, nonatomic) IBOutlet UILabel *damageLabel;
+@property (strong, nonatomic) IBOutlet UILabel *weaponLabel;
+@property (strong, nonatomic) IBOutlet UILabel *armorLabel;
+@property (strong, nonatomic) IBOutlet UILabel *storyLabel;
+
+// Direction button properties used to hide the button
+@property (strong, nonatomic) IBOutlet UIButton *northBtnProp;
+@property (strong, nonatomic) IBOutlet UIButton *eastBtnProp;
+@property (strong, nonatomic) IBOutlet UIButton *southBtnProp;
+@property (strong, nonatomic) IBOutlet UIButton *westBtnProp;
+
+
+- (IBAction)actionButton:(id)sender;
+- (IBAction)resetButton:(id)sender;
+- (IBAction)northButton:(id)sender;
+- (IBAction)eastButton:(id)sender;
+- (IBAction)southButton:(id)sender;
+- (IBAction)westButton:(id)sender;
 
 @end
